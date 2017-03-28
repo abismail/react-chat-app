5e4bd10bc80a8ec45fbcead0d3ab180efda6cd58
Abdul Ismail <abdul@signpost.co.za>
2017-03-28 23:02:48 +0200

    Added flows 1 and 2, email not working yet

--- server/main.js
+++ server/main.js
@@ -1,5 +1,57 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+import { xml2js } from 'meteor/peerlibrary:xml2js';
+import { Email } from 'meteor/email';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // import users
+  Users = new Mongo.Collection('users');
+  Meteor.call('importUsers', 'ismail29033@gmail.com,sakeenahroberts1@gmail.com', function(err, res) {
+  	var users_xml = xml2js.parseString( res, function(xmlerror, xmlresult){
+
+  		_.each(xmlresult.Users.User, function (user) {
+  			var insert_obj = {name: user.Name, surname:user.Surname, email:user.Email};
+  			console.log("inserting: " + insert_obj.name + ", " + insert_obj.surname + ", " + insert_obj.email);
+  			var user_id = null;
+  			
+  			var update_res = Users.update({email:user.Email}, insert_obj);
+  			
+  			if (update_res == 0) {
+  				user_id = Users.insert(insert_obj);
+  			}
+
+  			if( insert_obj.email == "ismail29033@gmail.com" ) {
+  				Meteor.call('sendEnrollEmail', [insert_obj.email, user_id], function(error, result){
+	  				if ( !result ) {
+	  					console.log("There was an error when trying to email: " + insert_obj.email );
+	  				}
+	  			});
+  			}
+  		} );
+
+  	} );
+  	var existing_users = Users.find({}, {email:"ismail29033@gmail.com"}).fetch();
+  	console.log( existing_users );
+  });
+});
+
+Meteor.methods({
+	'importUsers': function (emailList) {
+		this.unblock();
+		var users_api = "https://testapi.react.technology/users/?email="+emailList;
+		var response = HTTP.get( users_api ).content;
+
+		return response;
+	},
+	'sendEnrollEmail': function (email, user_id) {
+		check([email], [String]);
+
+		this.unblock();
+		Email.send({
+			to: email,
+			from: 'testapi@react.technology',
+			subject: 'Come Chat with the Ouens',
+			text: "To start chatting, set your password <a href='http://localhost:3000/setPassword.html?uid=" + user_id + "'>here</a> and login!"
+		});
+	}
 });
